90779a6f28f88e80b5b2ccd3f6407d730d8d78f7
pch8388 <pch838811@gmail.com>
2019-12-12 00:48:00 +0900

    [feat] : search method 구현

--- src/main/java/me/study/algo_1966/PrinterQueue.java
+++ src/main/java/me/study/algo_1966/PrinterQueue.java
@@ -1,5 +1,8 @@
 package me.study.algo_1966;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 public class PrinterQueue {
     public static void main(String[] args) {
         //TODO :
@@ -9,6 +12,38 @@ public class PrinterQueue {
     }
 
     public static int search(int[] arr, int start) {
-        return 0;
+        int maxValue = Arrays.stream(arr).max().getAsInt();
+        Queue<Node> list = new LinkedList<>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(new Node(i, arr[i]));
+        }
+
+        int result = 0;
+        while (true) {
+            Node current = list.poll();
+            if (current.data == maxValue) {
+                result++;
+                if (current.index == start) {
+                    return result;
+                } else {
+                    maxValue = list.stream()
+                                    .map(node -> node.data)
+                                    .max(Integer::compareTo)
+                                    .get();
+                }
+            } else {
+                list.add(current);
+            }
+        }
+    }
+
+    static class Node {
+        int index;
+        int data;
+
+        Node(int index, int data) {
+            this.index = index;
+            this.data = data;
+        }
     }
 }
--- src/test/java/me/study/algo_1966/PrinterQueueTests.java
+++ src/test/java/me/study/algo_1966/PrinterQueueTests.java
@@ -1,14 +1,23 @@
 package me.study.algo_1966;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PrinterQueueTests {
+
     @Test
+    @DisplayName("검색 테스트")
     public void search() {
         int[] arr = {1, 1, 9, 1, 1, 1};
         int result = PrinterQueue.search(arr, 0);
         assertThat(result).isEqualTo(5);
+
+        int[] arr2 = {1, 2, 3, 4};
+        assertThat(PrinterQueue.search(arr2, 2)).isEqualTo(2);
+
+        int[] arr3 = {5};
+        assertThat(PrinterQueue.search(arr3, 0)).isEqualTo(1);
     }
 }
