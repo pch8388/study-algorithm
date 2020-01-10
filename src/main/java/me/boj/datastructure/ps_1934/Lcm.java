aa40d09bd7bc793ba9f3d25a8db94053939df1da
pch8388 <pch838811@gmail.com>
2020-01-10 19:53:13 +0900

    [feat] : 최소공배수

---
+++ src/main/java/me/boj/datastructure/ps_1934/Lcm.java
@@ -0,0 +1,31 @@
+package me.boj.datastructure.ps_1934;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Lcm {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            builder.append((a * b) / gcd(a, b)).append("\n");
+        }
+        System.out.println(builder);
+        reader.close();
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
