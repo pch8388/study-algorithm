955f2bedcff9eaf8a9b97c2fa67adcf7bd18a1e7
pch8388 <pch838811@gmail.com>
2020-01-10 23:15:22 +0900

    [feat] : 골드바흐의 추측

---
+++ src/main/java/me/boj/datastructure/ps_6588/Goldbach.java
@@ -0,0 +1,48 @@
+package me.boj.datastructure.ps_6588;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Goldbach {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean[] booleans = new boolean[1000001];
+        int[] prime = new int[1000001];
+        int count = 0;
+
+        for (int i = 0; i <= 1000000; i++) {
+            booleans[i] = false;
+        }
+
+        for (int i = 2; i < 1000000; i++) {
+            if (!booleans[i]) {
+                prime[count++] = i;
+                for (int j = i + i; j <= 1000000; j += i) {
+                    booleans[j] = true;
+                }
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        while (true) {
+            boolean isFound = false;
+            int input = Integer.parseInt(reader.readLine());
+            if (input == 0) break;
+
+            for (int i = 0; i < input; i++) {
+                if (!booleans[input - prime[i]]) {
+                    isFound = true;
+                    builder.append(input).append(" = ").append(prime[i]).append(" + ").append(input - prime[i]).append("\n");
+                    break;
+                }
+            }
+            if (!isFound) {
+                builder.append(input).append(" = ").append("Goldbach's conjecture is wrong.\n");
+            }
+        }
+        System.out.println(builder);
+        reader.close();
+    }
+}
