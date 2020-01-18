41d29aa2b2e77a04c6fd4745049231e9e0ae218f
pch8388 <pch838811@gmail.com>
2020-01-18 23:50:46 +0900

    [refactor] : 1만들기 - 다시 풀어보기

--- src/main/java/me/boj/dp/ps_1463/One.java
+++ src/main/java/me/boj/dp/ps_1463/One.java
@@ -1,44 +1,38 @@
 package me.boj.dp.ps_1463;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class One {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
 
-        System.out.println(new Calculate(n).cal(n));
+    public static void main(String[] args) throws IOException {
+        // d[n] = 정수 N 을 3으로 나누거나, 2로 나누거나, 1을 빼서 1을 만드는 최소 횟수
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] d = new int[n + 1];
+
+        System.out.println(cal(n, d));
     }
 
-    static class Calculate {
-        int[] arr;
+    static int cal(int n, int[] d) {
+        if (n == 1) return 0;
 
-        Calculate(int n) {
-            arr = new int[n + 1];
+        if (d[n] > 0) return d[n];
+
+        d[n] = cal(n - 1, d) + 1;
+        if (n % 2 == 0) {
+            int temp = cal(n / 2, d) + 1;
+            if (d[n] > temp) d[n] = temp;
         }
 
-        int cal(int n) {
-            if (n == 1) return 0;
-
-            if(arr[n] > 0) return arr[n];
-
-            arr[n] = cal(n - 1) + 1;
-            if (n % 2 == 0) {
-                int temp = cal(n / 2) + 1;
-                if (arr[n] > temp) {
-                    arr[n] = temp;
-                }
-            }
-
-            if (n % 3 == 0) {
-                int temp = cal(n / 3) + 1;
-                if (arr[n] > temp) {
-                    arr[n] = temp;
-                }
-            }
-
-            return arr[n];
+        if (n % 3 == 0) {
+            int temp = cal(n / 3, d) + 1;
+            if (d[n] > temp) d[n] = temp;
         }
+
+        return d[n];
     }
-
 }
