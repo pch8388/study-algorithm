be46525957219e5f58e7f2ba40b72e6e5d8fb5eb
pch8388 <pch838811@gmail.com>
2020-02-10 23:14:09 +0900

    [refactor] 가장 긴 증가하는 부분수열 - 복습

--- src/test/java/me/boj/dp/ps_11053/MainTests.java
+++ src/test/java/me/boj/dp/ps_11053/MainTests.java
@@ -7,13 +7,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MainTests {
     @Test
     void test() {
-        int r1 = Main.calculate(6, new int[]{10, 20, 10, 30, 20, 50});
-        int r2 = Main.calculate(4, new int[]{1, 4, 2, 3});
-        int r3 = Main.calculate(7, new int[]{8, 9, 10, 1, 2, 3, 4});
-        int r4 = Main.calculate(7, new int[]{1, 4, 5, 2, 3, 4, 5});
-        int r5 = Main.calculate(1, new int[]{2});
-        int r6 = Main.calculate(4, new int[]{1, 4, 2, 3});
-        int r7 = Main.calculate(16, new int[]{8, 6, 9, 1, 4, 6, 7, 4, 3, 7, 4, 7, 2, 5, 2, 10});
+        int r1 = Main.solve(6, new int[]{10, 20, 10, 30, 20, 50});
+        int r2 = Main.solve(4, new int[]{1, 4, 2, 3});
+        int r3 = Main.solve(7, new int[]{8, 9, 10, 1, 2, 3, 4});
+        int r4 = Main.solve(7, new int[]{1, 4, 5, 2, 3, 4, 5});
+        int r5 = Main.solve(1, new int[]{2});
+        int r6 = Main.solve(4, new int[]{1, 4, 2, 3});
+        int r7 = Main.solve(16, new int[]{8, 6, 9, 1, 4, 6, 7, 4, 3, 7, 4, 7, 2, 5, 2, 10});
 
         assertThat(r1).isEqualTo(4);
         assertThat(r2).isEqualTo(3);
