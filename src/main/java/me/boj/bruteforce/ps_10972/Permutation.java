66fa90c631bae2a126132c84286b9c1c6490f1a4
pch8388 <pch838811@gmail.com>
2020-02-18 00:06:18 +0900

    [feat] 다음순열

---
+++ src/main/java/me/boj/bruteforce/ps_10972/Permutation.java
@@ -0,0 +1,52 @@
+package me.boj.bruteforce.ps_10972;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Permutation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        if (nextPermutation(n, arr)) {
+            for (int j = 0; j < n; j++) {
+                builder.append(arr[j]).append(" ");
+            }
+        } else {
+            builder.append(-1);
+        }
+        System.out.println(builder);
+    }
+
+    static boolean nextPermutation(int n, int[] arr) {
+        int i = n - 1;
+        while (i > 0 && arr[i - 1] >= arr[i]) i--;
+        if (i <= 0) return false;
+        int j = n - 1;
+        while (arr[j] <= arr[j - 1]) j--;
+
+        int temp = arr[i - 1];
+        arr[i - 1] = arr[j];
+        arr[j] = temp;
+
+        j = n - 1;
+        while (i < j) {
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+            i++; j--;
+        }
+
+        return true;
+    }
+}
