ccd61215dd200717d0fd8d9c82e1f496a9e71339
pch8388 <pch838811@gmail.com>
2020-03-24 00:02:12 +0900

    [feat] 단어 수학 풀이

--- src/main/java/me/boj/bruteforce/ps_1339/WordMath.java
+++ src/main/java/me/boj/bruteforce/ps_1339/WordMath.java
@@ -3,70 +3,53 @@ package me.boj.bruteforce.ps_1339;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
 
 public class WordMath {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
-        List<String> list = new ArrayList<>();
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            list.add(s);
-            for (int j = 0; j < s.length(); j++) {
-                map.put(s.charAt(j), 0);
-            }
-        }
-        int idx = map.keySet().size();
+        int N = Integer.parseInt(br.readLine());
 
+        char[] alphabet = new char[10];
+        int[] weight = new int[10];
 
-        arr = Arrays.copyOf(arr, idx);
-        Arrays.sort(arr);
-        int max = 0;
-        do {
-            Iterator<Character> it = map.keySet().iterator();
-            int index = idx;
-            while (it.hasNext()) {
-                map.replace(it.next(), arr[--index]);
-            }
+        Arrays.fill(alphabet, '0');
+        Arrays.fill(weight, 0);
 
-            int sum = 0;
-            for (String s : list) {
-                int l = s.length();
-                for (int j = 0; j < l; j++) {
-                    sum += map.get(s.charAt(j)) * (int) Math.pow(10, l - 1 - j);
+        for (int i = 0; i < N; i++) {
+
+            String numStr = br.readLine();
+
+            int w = 1;
+            for (int j = numStr.length() - 1; j >= 0; j--) {
+
+                char numChar = numStr.charAt(j);
+
+                for (int k = 0; k < 10; k++) {
+                    if (alphabet[k] == numChar) {
+                        weight[k] += w;
+                        break;
+                    } else if (alphabet[k] == '0') {
+                        alphabet[k] = numChar;
+                        weight[k] = w;
+                        break;
+                    }
                 }
+
+                w *= 10;
             }
-            max = Math.max(max, sum);
-
-        } while (nextPermutation(idx, arr));
-
-        System.out.println(max);
-    }
-
-    static boolean nextPermutation(int n, int[] arr) {
-        int i = n - 1;
-        while (i > 0 && arr[i - 1] >= arr[i]) i--;
-        if (i <= 0) return false;
-        int j = n - 1;
-        while (arr[j] <= arr[i - 1]) j--;
-
-        int temp = arr[i - 1];
-        arr[i - 1] = arr[j];
-        arr[j] = temp;
-
-        j = n - 1;
-        while (i < j) {
-            int tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-            i++;
-            j--;
         }
 
-        return true;
+        Arrays.sort(weight);
+
+        int sum = 0;
+        for (int i = 9; i >= 0; i--) {
+
+            sum += i * weight[i];
+        }
+
+        System.out.println(sum);
     }
 }
