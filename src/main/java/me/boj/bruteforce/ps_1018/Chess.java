f501b3793921cb782324150c8f0d88d17c190139
pch8388 <pch838811@gmail.com>
2020-04-30 23:29:18 +0900

    [feat] 체스판 다시 칠하기

---
+++ src/main/java/me/boj/bruteforce/ps_1018/Chess.java
@@ -0,0 +1,68 @@
+package me.boj.bruteforce.ps_1018;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.min;
+
+public class Chess {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        char[][] arr = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = s.charAt(j);
+            }
+        }
+
+        char[][] blackFirst = {
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}
+        };
+
+        char[][] whiteFirst = {
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}
+        };
+
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i + 7 < n; i++) {
+            for (int j = 0; j + 7 < m; j++) {
+                answer = min(answer, min(check(whiteFirst, arr, i, j), check(blackFirst, arr, i , j)));
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    static int check(char[][] board, char[][] arr, int si, int sj) {
+        int cnt = 0;
+        for (int i = si; i <= si + 7; i++) {
+            for (int j = sj; j <= sj + 7; j++) {
+                if (board[i - si][j - sj] != arr[i][j]) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
