193026793253a003b53bb87fe9337447c8da0423
pch8388 <pch838811@gmail.com>
2020-07-08 01:10:22 +0900

    [refactor] leetcode - Container with most water 반례에 대한 수정

--- src/main/java/me/leetcode/ContainerWithMostWater.java
+++ src/main/java/me/leetcode/ContainerWithMostWater.java
@@ -5,8 +5,8 @@ public class ContainerWithMostWater {
         int max = 0;
         int l = 0;
         int r = height.length - 1;
-        while (l < r) {
-            max = Math.max(max, Math.min(height[l], height[r]) * (r - 1));
+        while (l <= r) {
+            max = Math.max(max, Math.min(height[l], height[r]) * (r - l));
             if (height[l] < height[r]) l++;
             else r--;
         }
--- src/test/java/me/leetcode/ContainerWithMostWaterTest.java
+++ src/test/java/me/leetcode/ContainerWithMostWaterTest.java
@@ -10,7 +10,9 @@ class ContainerWithMostWaterTest {
     public void solve() {
         ContainerWithMostWater w = new ContainerWithMostWater();
         final int r1 = w.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7});
+        final int r2 = w.maxArea(new int[]{1, 1});
 
         assertThat(r1).isEqualTo(49);
+        assertThat(r2).isEqualTo(1);
     }
 }
