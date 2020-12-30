ef6387d3596f6bb503e0802d49fb212b87cbd363
pch8388 <pch838811@gmail.com>
2020-12-31 00:40:09 +0900

    [refactor] 고민 후 다시 풀이

--- src/main/java/me/programmers/etc/ps_42577/PhoneBook.java
+++ src/main/java/me/programmers/etc/ps_42577/PhoneBook.java
@@ -1,27 +1,12 @@
 package me.programmers.etc.ps_42577;
 
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
 
 public class PhoneBook {
     public boolean solution(String[] phone_book) {
-
-        Arrays.sort(phone_book, Comparator.comparing(String::length));
-        Map<String, Boolean> book = new HashMap<>();
-        for (String number : phone_book) {
-            if (book.size() == 0) {
-                book.put(number, true);
-                continue;
-            }
-
-            for (int i = 0; i < number.length() - 1; i++) {
-                String sub = number.substring(0, i + 1);
-                if (book.containsKey(sub)) return false;
-            }
-
-            book.put(number, true);
+        Arrays.sort(phone_book);
+        for (int i = 1; i < phone_book.length; i++) {
+            if (phone_book[i].startsWith(phone_book[i - 1])) return false;
         }
         return true;
     }
