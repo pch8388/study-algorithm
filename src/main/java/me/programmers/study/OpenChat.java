20dbebe441d5545b7ea0c41d1bca6fa87f172df6
SC <pch838811@gmail.com>
2021-12-19 23:26:05 +0900

    [refactor] programmers - 오픈채팅방

--- src/main/java/me/programmers/study/OpenChat.java
+++ src/main/java/me/programmers/study/OpenChat.java
@@ -3,49 +3,60 @@ package me.programmers.study;
 import java.util.*;
 public class OpenChat {
     public String[] solution(String[] record) {
-        Queue<Info> result = new LinkedList<>();
-        Map<String, String> nameStorage = new HashMap<>();
-        for (String row : record) {
-            String[] arr = row.split(" ");
-            if("Leave".equals(arr[0])) {
-                result.add(new Leave(arr[1]));
-            } else if ("Enter".equals(arr[0])) {
-                result.add(new Enter(arr[1]));
-                nameStorage.put(arr[1], arr[2]);
-            } else {
-                nameStorage.put(arr[1], arr[2]);
+        Room room = new Room(record);
+        return room.getLogs();
+    }
+
+    static class Room {
+        Queue<Info> infoLogs = new LinkedList<>();
+        Map<String, String> names = new HashMap<>();
+        Room(String[] record) {
+            for (String row : record) {
+                String[] strings = row.split(" ");
+                if("Leave".equals(strings[0])) {
+                    infoLogs.add(new Leave(strings[1]));
+                }
+                if ("Enter".equals(strings[0])) {
+                    infoLogs.add(new Enter(strings[1]));
+                    names.put(strings[1], strings[2]);
+                }
+                if ("Change".equals(strings[0])){
+                    names.put(strings[1], strings[2]);
+                }
             }
         }
 
-        return result.stream()
-            .map(info -> info.print(nameStorage))
-            .toArray(String[]::new);
+        public String[] getLogs() {
+            return infoLogs.stream()
+                .map(info -> info.print(names))
+                .toArray(String[]::new);
+        }
     }
 
     static class Enter implements Info {
-        private String id;
+        private final String id;
 
         Enter(String id) {
             this.id = id;
         }
-        public String print(Map<String, String> nameStorage) {
-            return nameStorage.get(id) + "님이 들어왔습니다.";
+        public String print(Map<String, String> names) {
+            return names.get(id) + "님이 들어왔습니다.";
         }
     }
 
     static class Leave implements Info {
-        private String id;
+        private final String id;
 
         Leave(String id) {
             this.id = id;
         }
 
-        public String print(Map<String, String> nameStorage) {
-            return nameStorage.get(id) + "님이 나갔습니다.";
+        public String print(Map<String, String> names) {
+            return names.get(id) + "님이 나갔습니다.";
         }
     }
 }
 
 interface Info {
-    String print(Map<String, String> nameStorage);
+    String print(Map<String, String> names);
 }
