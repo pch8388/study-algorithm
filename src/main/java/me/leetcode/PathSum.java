9d9d6e68fd646e0c0060f1ce975d5113e7e31a3b
SC <pch838811@gmail.com>
2021-12-25 19:45:15 +0900

    [refactor] leetcode -  PathSum

--- src/main/java/me/leetcode/PathSum.java
+++ src/main/java/me/leetcode/PathSum.java
@@ -2,32 +2,17 @@ package me.leetcode;
 
 public class PathSum {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return dfs(root, targetSum, 0);
-    }
-
-    boolean dfs(TreeNode root, int targetSum, int sum) {
         if (root == null) {
             return false;
         }
 
-        int sumVal = sum + root.val;
-
         // leaf
         if (root.left == null && root.right == null) {
-            return targetSum == sumVal;
+            return targetSum - root.val == 0;
         }
 
-        boolean isLeftSum = false;
-        if (root.left != null) {
-            isLeftSum = dfs(root.left, targetSum, sumVal);
-        }
-
-        boolean isRightSum = false;
-        if (root.right != null) {
-            isRightSum = dfs(root.right, targetSum, sumVal);
-        }
-
-        return isLeftSum || isRightSum;
+        return hasPathSum(root.left, targetSum - root.val)
+            || hasPathSum(root.right, targetSum - root.val);
     }
 
     static class TreeNode {
